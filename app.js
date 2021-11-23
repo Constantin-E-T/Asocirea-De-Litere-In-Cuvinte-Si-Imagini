b111598299beb5a710e0a03c86f43158bba937de
Emilian Constantin <emiliantivlica@yahoo.com>
2021-11-23 22:18:38 +0000

    second commit

---
+++ app.js
@@ -0,0 +1,217 @@
+function a() {
+    document.getElementById("a");
+    document.getElementById("letter").innerHTML = "Aa";
+    document.getElementById("word").innerHTML = "Albina";
+    document.getElementById("picture").innerHTML =
+      "<iframe id='picture' src='https://giphy.com/embed/QvYYIQ8GIu9XIoTcHp' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function ă() {
+    document.getElementById("ă");
+    document.getElementById("letter").innerHTML = "Ăă";
+    document.getElementById("word").innerHTML = "Ăla";
+    document.getElementById("picture").innerHTML =
+      "<iframe id='picture' src='https://giphy.com/embed/7zGiqObEPFAjTJxdZn' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function â() {
+    document.getElementById("â");
+    document.getElementById("letter").innerHTML = "Ââ";
+    document.getElementById("word").innerHTML = "Ăla";
+    document.getElementById("picture").innerHTML =
+      "<iframe id='picture' src='https://giphy.com/embed/7zGiqObEPFAjTJxdZn' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function b() {
+    document.getElementById("b");
+    document.getElementById("letter").innerHTML = "Bb";
+    document.getElementById("word").innerHTML = "Buburuză";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/xwojSpw9KulcK11cuV' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function c() {
+    document.getElementById("c");
+    document.getElementById("letter").innerHTML = "Cc";
+    document.getElementById("word").innerHTML = "Cățel";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/Crb7Qs3Wun15u' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function d() {
+    document.getElementById("d");
+    document.getElementById("letter").innerHTML = "Dd";
+    document.getElementById("word").innerHTML = "Delfin";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/eKI2Dw3roPCUPnYu4Z' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function e() {
+    document.getElementById("e");
+    document.getElementById("letter").innerHTML = "Ee";
+    document.getElementById("word").innerHTML = "Elefant";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/z7NbuRgGB8H5zSnkY5' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function f() {
+    document.getElementById("f");
+    document.getElementById("letter").innerHTML = "Ff";
+    document.getElementById("word").innerHTML = "Fluture";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/3o7WIsnMAwUapnyXOU' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function g() {
+    document.getElementById("g");
+    document.getElementById("letter").innerHTML = "Gg";
+    document.getElementById("word").innerHTML = "Găină";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/v8Vux9WxXcnXEnOGn1' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function h() {
+    document.getElementById("h");
+    document.getElementById("letter").innerHTML = "Hh";
+    document.getElementById("word").innerHTML = "Hipopotam";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/ityWFwWRr5FIzTxfKB' width='200' height='200' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function i() {
+    document.getElementById("i");
+    document.getElementById("letter").innerHTML = "Ii";
+    document.getElementById("word").innerHTML = "Inel";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/cPSn4L1n0peqAgFMw6' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function î() {
+    document.getElementById("î");
+    document.getElementById("letter").innerHTML = "Îî";
+    document.getElementById("word").innerHTML = "Înghețată";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/Zs92908g88ppxzp84u' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function j() {
+    document.getElementById("j");
+    document.getElementById("letter").innerHTML = "Jj";
+    document.getElementById("word").innerHTML = "Jucărie";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/QSiSKe0Hyd8rxRsw7T' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function k() {
+    document.getElementById("k");
+    document.getElementById("letter").innerHTML = "Kk";
+    document.getElementById("word").innerHTML = "Kiwi";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/MepIfXlsKTz4Gagake' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function l() {
+    document.getElementById("l");
+    document.getElementById("letter").innerHTML = "Ll";
+    document.getElementById("word").innerHTML = "Leu";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/zeoxV7uCUCqt2' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function m() {
+    document.getElementById("m");
+    document.getElementById("letter").innerHTML = "Mm";
+    document.getElementById("word").innerHTML = "Măr";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/J1R6LF0c9df6XyKkaU' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function n() {
+    document.getElementById("n");
+    document.getElementById("letter").innerHTML = "Nn";
+    document.getElementById("word").innerHTML = "Nuci";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/0Sz5MTRnDSuzFN0i7q' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function o() {
+    document.getElementById("o");
+    document.getElementById("letter").innerHTML = "Oo";
+    document.getElementById("word").innerHTML = "Ochelari";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/j2dkYkY3YjtMA' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function p() {
+    document.getElementById("p");
+    document.getElementById("letter").innerHTML = "Pp";
+    document.getElementById("word").innerHTML = "Pălărie";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/TgIqMv5ofWSZ1mQMNa' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function q() {
+    document.getElementById("q");
+    document.getElementById("letter").innerHTML = "Qq";
+    document.getElementById("word").innerHTML = "Qou";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/5rz2R5aCwuF6CdkCSt' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function r() {
+    document.getElementById("r");
+    document.getElementById("letter").innerHTML = "Rr";
+    document.getElementById("word").innerHTML = "Rachetă";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/XGVQmEcg9cR5WycSG0' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function s() {
+    document.getElementById("s");
+    document.getElementById("letter").innerHTML = "Ss";
+    document.getElementById("word").innerHTML = "Soare";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/LhTDHdt8yzoA1chbqr' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function ș() {
+    document.getElementById("ș");
+    document.getElementById("letter").innerHTML = "Șș";
+    document.getElementById("word").innerHTML = "Șarpe";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/3oKIPCohynIR4gBdhm' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function t() {
+    document.getElementById("t");
+    document.getElementById("letter").innerHTML = "Tt";
+    document.getElementById("word").innerHTML = "Tractor";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/iWSZgLOBFui1RXHz97' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function ț() {
+    document.getElementById("ț");
+    document.getElementById("letter").innerHTML = "Țț";
+    document.getElementById("word").innerHTML = "Țânțar";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/P62oN0PTj3xLioFUms' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function u() {
+    document.getElementById("u");
+    document.getElementById("letter").innerHTML = "Uu";
+    document.getElementById("word").innerHTML = "Urs";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/kf3EjrAsKp3P9bhYHG' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function v() {
+    document.getElementById("v");
+    document.getElementById("letter").innerHTML = "Vv";
+    document.getElementById("word").innerHTML = "Veveriță";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/WOHB8mWT7GBYd7zWTj' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function w() {
+    document.getElementById("w");
+    document.getElementById("letter").innerHTML = "Ww";
+    document.getElementById("word").innerHTML = "Walkman";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/B5o7vq5RZM4YqfeDcV' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function x() {
+    document.getElementById("x");
+    document.getElementById("letter").innerHTML = "Xx";
+    document.getElementById("word").innerHTML = "Xilofon";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/3oFzmb4c6NDjHUZD3O' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function y() {
+    document.getElementById("y");
+    document.getElementById("letter").innerHTML = "Yy";
+    document.getElementById("word").innerHTML = "Yală";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/d62ljXt9hoVppQLS42' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
+  function z() {
+    document.getElementById("z");
+    document.getElementById("letter").innerHTML = "Zz";
+    document.getElementById("word").innerHTML = "Zebră";
+    document.getElementById("picture").innerHTML =
+    "<iframe id='picture' src='https://giphy.com/embed/1CYRpgPBc8yo8' width='150' height='150' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>";
+  }
